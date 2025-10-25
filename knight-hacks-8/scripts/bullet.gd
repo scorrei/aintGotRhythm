8db108315859db52dcfd7e22fbf2d6e6d3339ff2
scorrei <correia.samuel@protonmail.com>
2025-10-25 15:03:24 -0400

    bullets now spawn on button press

--- knight-hacks-8/scripts/bullet.gd
+++ knight-hacks-8/scripts/bullet.gd
@@ -1,6 +1,11 @@
 extends Sprite2D
 
 @export var speed: float = 3.0
+var initial_x: float = 200
+
+
+func _init():
+	set_process(false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -9,3 +14,6 @@ func _process(delta: float) -> void:
 	if global_position.x < -300 and not $Timer.is_stopped():
 		print($Timer.time_left)
 		$Timer.stop()
+func setBullet(target_y: float):
+	global_position = Vector2(initial_x, target_y)
+	set_process(true)
--- knight-hacks-8/scripts/bullet_catcher.gd
+++ knight-hacks-8/scripts/bullet_catcher.gd
@@ -1,8 +1,15 @@
 extends Sprite2D
-
+@onready var bullet = preload("res://obects/bullet.tscn")
 @export var button_name: String = ""
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(button_name):
-		print(button_name)
+		CreateBullet()
+
+func CreateBullet():
+	var bullet_inst = bullet.instantiate()
+	get_tree().get_root().call_deferred("add_child", bullet_inst)
+	bullet_inst.setBullet(position.y)
